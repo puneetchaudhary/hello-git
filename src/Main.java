c651786162be5aa90a44e789dbbe6e1f86d2aa3b
datreon <timepass.datreon@gmail.com>
2017-10-15 16:05:41 +0530

    initial commit(hello my world by varun)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("hello world");
+        System.out.println("hello my world by varun");
+
     }
 }
